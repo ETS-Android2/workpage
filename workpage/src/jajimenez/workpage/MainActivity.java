38622d955458228af8b414c4c71a3b87bb35aaca
Jose A. Jimenez <jajimenezcarm@gmail.com>
2014-11-05 20:48:05 +0100

    "About" activity added.

--- workpage/src/jajimenez/workpage/MainActivity.java
+++ workpage/src/jajimenez/workpage/MainActivity.java
@@ -2,11 +2,28 @@ package jajimenez.workpage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    public void onAboutItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
