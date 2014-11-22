ccfcb23530dcdfaaec7e6b087e071afe2e0fa80a
Jose A. Jimenez <jajimenezcarm@gmail.com>
2014-11-23 00:36:28 +0100

    Class for converting different date-times added.

---
+++ workpage/src/jajimenez/workpage/logic/DateTimeTool.java
@@ -0,0 +1,47 @@
+package jajimenez.workpage.logic;
+
+import java.util.Calendar;
+import java.text.DateFormat;
+
+public class DateTimeTool {
+    public String getIso8601DateTime(Calendar calendar) {
+        String date = null;
+
+        if (calendar != null) {
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+            date = String.format("%d-%02d-%02d 00:00:00.000", year, month, day);
+        }
+
+        return date; 
+    }
+
+    public Calendar getCalendar(String iso8601DateTime) {
+        Calendar calendar = null;
+        
+        if (iso8601DateTime != null && !iso8601DateTime.equals("")) {
+            calendar = Calendar.getInstance();
+
+            int year = Integer.valueOf(iso8601DateTime.substring(0, 4));
+            int month = Integer.valueOf(iso8601DateTime.substring(5, 7));
+            int day = Integer.valueOf(iso8601DateTime.substring(8, 10));
+
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+            calendar.set(Calendar.HOUR_OF_DAY, 0);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 0);
+            calendar.set(Calendar.MILLISECOND, 0);
+        }
+
+        return calendar;
+    }
+
+    public String getInterfaceFormattedDate(Calendar calendar) {
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        return dateFormat.format(calendar.getTime());
+    }
+}
