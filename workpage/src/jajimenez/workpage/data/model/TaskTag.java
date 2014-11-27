81582e9597f44c6f11a8ac1d26f482646202199c
Jose A. Jimenez <jajimenezcarm@gmail.com>
2014-11-27 13:08:09 +0100

    Naming error fix.

--- workpage/src/jajimenez/workpage/data/model/TaskTag.java
+++ workpage/src/jajimenez/workpage/data/model/TaskTag.java
@@ -1,36 +1,36 @@
 package jajimenez.workpage.data.model;
 
 public class TaskTag extends Entity {
-    private long workspaceId;
+    private long contextId;
     private String name;
     private long order;
 
     public TaskTag() {
         super();
 
-        workspaceId = -1;
+        contextId = -1;
         name = "";
         order = 0;
     }
 
-    public TaskTag(long workspaceId, String name, long order) {
+    public TaskTag(long contextId, String name, long order) {
         super();
 
-        this.workspaceId = workspaceId;
+        this.contextId = contextId;
         this.name = name;
         this.order = order;
     }
 
-    public TaskTag(long id, long workspaceId, String name, long order) {
+    public TaskTag(long id, long contextId, String name, long order) {
         super(id);
 
-        this.workspaceId = workspaceId;
+        this.contextId = contextId;
         this.name = name;
         this.order = order;
     }
 
-    public long getWorkspaceId() {
-        return workspaceId;
+    public long getContextId() {
+        return contextId;
     }
 
     public String getName() {
@@ -41,8 +41,8 @@ public class TaskTag extends Entity {
         return order;
     }
 
-    public void setWorkspaceId(long workspaceId) {
-        this.workspaceId = workspaceId;
+    public void setContextId(long contextId) {
+        this.contextId = contextId;
     }
 
     public void setName(String name) {
