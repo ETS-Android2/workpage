f2412168d435d3bd073bda52fc028195496ae534
Jose A. Jimenez <jajimenezcarm@gmail.com>
2017-09-15 14:09:52 +0200

    Main activity menu removed.

--- workpage/app/src/main/java/jajimenez/workpage/MainActivity.java
+++ workpage/app/src/main/java/jajimenez/workpage/MainActivity.java
@@ -276,23 +276,18 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
-    }
+    }*/
 
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        }
-        else {
-            super.onBackPressed();
-        }
+        if (drawer.isDrawerOpen(GravityCompat.START)) drawer.closeDrawer(GravityCompat.START);
+        else super.onBackPressed();
     }
 
     @Override
